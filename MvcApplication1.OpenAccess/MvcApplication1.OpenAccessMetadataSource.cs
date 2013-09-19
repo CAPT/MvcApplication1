21fc2614997d787bfeece3a12a527660d6b2d9a7
SASHA <_CAPT_@mail.ru>
2013-09-20 00:12:35 +0400

    00
    Upload 1st

---
+++ MvcApplication1.OpenAccess/MvcApplication1.OpenAccessMetadataSource.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Telerik.OpenAccess.Metadata;
+using Telerik.OpenAccess.Metadata.Fluent;
+
+namespace MvcApplication1.OpenAccess
+{
+    public class MvcApplication1OpenAccessMetadataSource : FluentMetadataSource
+    {
+        protected override IList<MappingConfiguration> PrepareMapping()
+        {
+            // Getting Started with the Fluent Mapping API
+            // http://www.telerik.com/help/openaccess-orm/fluent-mapping-overview.html
+
+            List<MappingConfiguration> configurations = new List<MappingConfiguration>();
+
+            MappingConfiguration<Product> productConfiguration = new MappingConfiguration<Product>();
+            productConfiguration.MapType(x => new
+            {
+                ID = x.ID,
+                Price = x.Price,
+                ProductName = x.ProductName
+            }).ToTable("Products");
+            productConfiguration.HasProperty(x => x.ID).IsIdentity(KeyGenerator.Autoinc);
+
+            configurations.Add(productConfiguration);
+
+            return configurations;
+        }
+    }
+}
---
+++ MvcApplication1Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MvcApplication1Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
